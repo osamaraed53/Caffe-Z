2062403ad939bdb04a930d70f2a612baca786d98
osamaraed53 <osamaraed53@gmail.com>
2023-10-02 21:53:51 +0300

    newcommit

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 const main = document.querySelector('#main');
 
 let name = prompt('Enter your name please:');
-
-let gender = prompt('Enter your gender (male or female) please:');
 let title = " ";
+let gender = "  " ;
 
-if (gender === 'male') {
-    title = "Mr.";
-    alert(`Welcome ${title} ${name}!`);
-}else if (gender === 'female') {
-    title = "Ms.";
-    alert(`Welcome ${title} ${name}!`);
-} else {
-    alert(`Welcome ${title} ${name}!`);
 
+function checkGender(){
+    gender = prompt('Enter your gender (male or female) please:');
+    let condition = true;
+    while (condition) {
+    gender = gender.toLowerCase();
+    if (gender === 'male') {
+        title = 'Mr.';
+        alert(`Welcome ${title} ${name}!`);
+        condition = false;
+    } else if (gender === 'female') {
+        title = 'Ms.';
+        alert(`Welcome ${title} ${name}!`);
+
+        condition = false;
+    } else {
+        gender = prompt('Enter your gender (male or female) *not correct*: ');
+    }
+    }
 }
+
+checkGender();
 
 let drinkType = prompt(`${title} ${name} ,hot or cold ?`);
 
-let drinkName = prompt(` ${title} ${name} ,What is the drink do you want?`); 
+let drinkName = prompt(` ${title} ${name} ,What is the drink do you want?`);
+
+const arr=[(title+name),gender,drinkType,drinkName]
 
 alert(`Your ${drinkType} ${drinkName}  is getting prepared`);
 
-console.log(`User ${name} ordered a ${drinkType} ${drinkName}.`);
+// console.log(`User ${name} ordered a ${drinkType} ${drinkName}.`);
+for (let index = 0; index < arr.length; index++) {
+    console.log(arr[index]);
+    
+}
+
+
+
+
+
+
