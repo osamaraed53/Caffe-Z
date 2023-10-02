e7872b5c69267bf5de24575baa15c483ea7a1190
osamaraed53 <osamaraed53@gmail.com>
2023-10-02 23:44:36 +0300

    ex2

--- app.js
+++ app.js
@@ -5,27 +5,24 @@ let title = " ";
 let gender = "  " ;
 
 
-function checkGender(){
-    gender = prompt('Enter your gender (male or female) please:');
-    let condition = true;
-    while (condition) {
-    gender = gender.toLowerCase();
-    if (gender === 'male') {
-        title = 'Mr.';
-        alert(`Welcome ${title} ${name}!`);
-        condition = false;
-    } else if (gender === 'female') {
-        title = 'Ms.';
-        alert(`Welcome ${title} ${name}!`);
-
-        condition = false;
-    } else {
-        gender = prompt('Enter your gender (male or female) *not correct*: ');
-    }
-    }
+gender = prompt('Enter your gender (male or female) please:');
+let condition = true;
+while (condition) {
+gender = gender.toLowerCase();
+if (gender === 'male') {
+    title = 'Mr.';
+    alert(`Welcome ${title} ${name}!`);
+    condition = false;
+} else if (gender === 'female') {
+    title = 'Ms.';
+    alert(`Welcome ${title} ${name}!`);
+    condition = false;
+} else {
+    gender = prompt('Enter your gender (male or female) *not correct*: ');
+}
 }
 
-checkGender();
+
 
 let drinkType = prompt(`${title} ${name} ,hot or cold ?`);
 
