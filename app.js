673a5021bd2cef92ce7d205fcb91457d7fbb1205
osamaraed53 <osamaraed53@gmail.com>
2023-10-03 19:30:22 +0300

    first commit for change in Dom

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-const main = document.querySelector('#main');
-
 let name = prompt('Enter your name please:');
 let title = " ";
 let gender = "  " ;
@@ -31,18 +29,47 @@ let drinkType = prompt(`${title} ${name} ,hot or cold ?`);
 
 let drinkName = prompt(` ${title} ${name} ,What is the drink do you want?`);
 
-const arr=[(title+name),gender,drinkType,drinkName]
+const arr=[(title+name),gender,drinkType,drinkName];
 
 alert(`Your ${drinkType} ${drinkName}  is getting prepared`);
 
-// console.log(`User ${name} ordered a ${drinkType} ${drinkName}.`);
+console.log(`User ${name} ordered a ${drinkType} ${drinkName}.`);
 for (let index = 0; index < arr.length; index++) {
     console.log(arr[index]);
     
-}
+};
+
+
+//create div in index page 
+const divElement = document.createElement("div");
+document.body.appendChild(divElement);
+// create paragraphElement in top of div
+const paragraphElement = document.createElement("p");
+paragraphElement.textContent= arr[0];
+divElement.appendChild(paragraphElement);
+
+// create ul in div 
+const ulElement = document.createElement("ul");
+divElement.appendChild(ulElement);
+
+// create list
+let liElement = document.createElement("li");
+liElement.textContent= `Gender ${arr[1]}`;
+ulElement.appendChild(liElement);
+liElement = document.createElement("li");
+liElement.textContent= `Drink Type : ${arr[2]}`;
+ulElement.appendChild(liElement);
+liElement = document.createElement("li");
+liElement.textContent= `Drink Name ${arr[3]}`;
+ulElement.appendChild(liElement);
 
 
 
 
 
 
+// arr.forEach((item) => {
+//   const paragraphElement = document.createElement("p");
+//   paragraphElement.textContent = item;
+//   cDiv.appendChild(paragraphElement);
+// });
